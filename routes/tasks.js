1779a2106464f0acba4e5a5bb211c1b1c6f0f293
Santiago <sochoav8a@gmail.com>
2023-09-06 17:57:56 -0500

    fin fismat

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
+////ENDPOINTS
 
+//Get all taks
 router.get('/', (req, res) => {
   db.all("SELECT * FROM tasks", (err, rows) => {
     if(err) {
@@ -10,7 +12,52 @@ router.get('/', (req, res) => {
     res.json(rows);
   });
 });
+///Post a task
+router.post('/', (req, res) => {
+  const { name, description, dueDate } = req.body;
+  const stmt = db.prepare("INSERT INTO tasks (name, description, dueDate) VALUES (?, ?, ?)");
+  stmt.run(name, description, dueDate, (err) => {
+      if(err) {
+          return res.status(500).json(err);
+      }
+      res.json({ message: 'Task created successfully!' });
+  });
+});
+///Get a task by id
+router.get('/:id', (req, res) => {
+  const taskId = req.params.id;
+  db.get("SELECT * FROM tasks WHERE id = ?", [taskId], (err, row) => {
+      if(err) {
+          return res.status(500).json(err);
+      }
+      res.json(row);
+  });
+});
 
-// Añadirá otros endpoints más adelante...
+///Update a task by ID
+router.put('/:id', (req, res) => {
+  const taskId = req.params.id;
+  const { name, description, dueDate } = req.body;
+  const stmt = db.prepare("UPDATE tasks SET name = ?, description = ?, dueDate = ? WHERE id = ?");
+  stmt.run(name, description, dueDate, taskId, (err) => {
+      if(err) {
+          return res.status(500).json(err);
+      }
+      res.json({ message: 'Task updated successfully!' });
+  });
+});
+
+///Delete a task
+router.delete('/:id', (req, res) => {
+  const taskId = req.params.id;
+  const stmt = db.prepare("DELETE FROM tasks WHERE id = ?");
+  stmt.run(taskId, (err) => {
+      if(err) {
+          return res.status(500).json(err);
+      }
+      res.json({ message: 'Task deleted successfully!' });
+  });
+});
+
 
 module.exports = router;
